57cfdf3ead0a4624994919f9409b17cc83cd7c43
min jung <min.jung@kcl.ac.uk>
2022-10-10 11:43:25 +0100

    fix indentation

--- things/models.py
+++ things/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.core.validators
-    import MinValueValidator, MaxValueValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, MaxLengthValidator
 
 # Create your models here.
 
